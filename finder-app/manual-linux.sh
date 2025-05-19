513b1ad756503f141078d91fad16d745ef65a104
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2025-05-19 11:13:35 +0200

    Assingment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+WRITER_DIR=$(pwd)
 
 if [ $# -lt 1 ]
 then
@@ -35,9 +36,14 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} 
 fi
 
 echo "Adding the Image in outdir"
+cd "$OUTDIR"
+cp ./linux-stable/arch/arm64/boot/Image ./Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,33 +54,52 @@ then
 fi
 
 # TODO: Create necessary base directories
-
 cd "$OUTDIR"
+mkdir -p rootfs/{bin,sbin,etc,proc,sys,usr/{bin,sbin,lib},lib,lib64,dev,home,tmp,var/log,var}
+
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+cd "$OUTDIR"/rootfs
+${CROSS_COMPILE}readelf -a /usr/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a /usr/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp -r ${SYSROOT}/lib ${OUTDIR}/rootfs
+cp -r ${SYSROOT}/lib64 ${OUTDIR}/rootfs
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/tty c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${WRITER_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -r * ${OUTDIR}/rootfs/home
+cp -r ../conf ${OUTDIR}/rootfs
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root | gzip -f > ${OUTDIR}/initramfs.cpio.gz
