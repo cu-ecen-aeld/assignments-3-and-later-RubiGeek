884cd35b93b567f25e9563f1b327536f93cf1256
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2025-05-20 09:51:38 +0200

    Assingment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +15,29 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc = 0;
+    if (thread_func_args == NULL) {
+        ERROR_LOG("Thread function received NULL thread_func_args");
+        pthread_exit(NULL);
+    }
+    thread_func_args->thread_id = pthread_self();
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc != 0) {
+        ERROR_LOG("Failed to lock mutex, error code: %d", rc);
+        thread_func_args->thread_complete_success = false;
+        pthread_exit(NULL);
+    }
+    usleep(thread_func_args->wait_to_release_ms*1000);
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if (rc != 0) {
+        ERROR_LOG("Failed to unlock mutex, error code: %d", rc);
+        thread_func_args->thread_complete_success = false;
+        pthread_exit(NULL);
+    }
+    
+    thread_func_args->thread_complete_success = true;   
     return thread_param;
 }
 
@@ -28,6 +52,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *data = malloc(sizeof(struct thread_data));
+    if (data == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+    
+
+    int rc = pthread_create(thread, PTHREAD_CREATE_JOINABLE, threadfunc, data);
+    if (rc != 0) {
+        ERROR_LOG("Failed to create thread, error code: %d", rc);
+        free(data);
+        return false;
+    } 
+    //free(data);
+    return true;
 }
 
